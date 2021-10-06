1eb4a9efe3b3a05752afe0c6b8c6d089e982e3e4
Fabrizzio Lo Presti <68297385+Fabrox14@users.noreply.github.com>
2021-10-06 15:55:37 -0300

    Modelo Parcial Completo

---
+++ Modelo de parcial/ParcialSln/ParcialApp/Acceso a datos/FacturaDAO.cs
@@ -0,0 +1,29 @@
+﻿using ParcialApp.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialApp.Acceso_a_datos
+{
+    class FacturaDAO : IFacturaDao
+    {
+        public DataTable ListarProductos()
+        {
+            return HelperDAO.ObtenerInstancia().ConsultaSQL("SP_CONSULTAR_PRODUCTOS");
+        }
+
+
+        public int ObtenerProximoNroPresupuesto()
+        {
+            return HelperDAO.ObtenerInstancia().ProximoID("SP_PROXIMO_ID", "@next");
+        }
+
+        public bool Crear(Factura oFactura)
+        {
+            return HelperDAO.ObtenerInstancia().Save(oFactura);
+        }
+    }
+}
